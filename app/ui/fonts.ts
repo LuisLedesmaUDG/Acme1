0ed4e6d39f08277075739c95b48840a8ad9eeafe
LuisLedesmaUDG <113808590+LuisLedesmaUDG@users.noreply.github.com>
2024-05-02 16:40:05 -0600

    Acme 2

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Sevillana} from 'next/font/google';
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google';
+
+export const lusitana = Lusitana({subsets:['latin'], weight: "400"});
+export const sevillana = Sevillana({subsets:['latin'], weight: "400"});
+export const inter = Inter({subsets:['latin']});
